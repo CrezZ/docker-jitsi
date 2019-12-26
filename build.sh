7e2c5b7bd29b938adb9cfc646bf3622457597508
root <unpk@mail.ru>
2019-12-26 16:22:45 +0300

    change build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,4 @@
 
 docker build -t jitsi2 . && \
 docker build -t jibri2 jibri/  && \
-#docker-compose -f my-docker-compose.yaml up -d
 docker-compose -f my-docker-compose.yaml up -d
-
-
-
-#docker-compose -f my-docker-compose.yaml up -d
---
+++ my-build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+docker build -t jitsi2 . && \
+docker build -t jibri2 jibri/  && \
+#docker-compose -f my-docker-compose.yaml up -d
+docker-compose -f my-docker-compose.yaml up -d
+
+
+
+#docker-compose -f my-docker-compose.yaml up -d
